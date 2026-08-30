1c27ced012c3193ba8d72f7cf97ea8c67715044c
Chanel Yumi <314467409+ChanelYumi@users.noreply.github.com>
2026-08-30 08:35:52 -0300

    Atividade 12

---
+++ Lista 1/atividade12.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+
+    float n1, n2, n1Elevadon2, n2Elevadon1;
+   
+    printf("Digite um número maior que 0: ");
+    scanf("%f", &n1);
+
+    if (n1 > 0) {
+        printf("Digite outro número maior que 0: ");
+        scanf("%f", &n2);
+
+        if (n2 > 0) {
+
+            n1Elevadon2 = pow(n1, n2);
+            n2Elevadon1 = pow(n2, n1);
+
+            printf("n1 elevado a n2, é: %.2f\n", n1Elevadon2 );
+            printf("n2 elevado a n1, é: %.2f\n", n2Elevadon1 );
+
+        }   else {
+            printf("Tente novamente!\n ");
+        }
+    } else {
+        printf("Tente novamente!\n ");
+    }
+    return 0;
+}
