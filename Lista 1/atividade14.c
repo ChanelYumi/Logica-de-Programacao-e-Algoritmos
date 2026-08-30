c5a033483694e68ccdc4c8cd4bb468a5910fa69b
Chanel Yumi <314467409+ChanelYumi@users.noreply.github.com>
2026-08-30 14:10:06 -0300

    Atividade 14

---
+++ Lista 1/atividade14.c
@@ -0,0 +1,25 @@
+/*
+Faça um programa que receba o ano de nascimento de uma pessoa e o ano atual. Calcule e mostre:
+a) A idade dessa pessoa.
+b) Quantos anos essa pessoa terá em 2050.
+*/
+
+#include <stdio.h>
+int main() {
+
+    int anoNascimento, anoAtual, idade, idade2050;
+   
+    printf("Digite o ano de nascimento: ");
+    scanf("%d", &anoNascimento);
+    printf("Digite o ano atual: ");
+    scanf("%d", &anoAtual);
+
+    idade = anoAtual - anoNascimento;
+    idade2050 = 2050 - anoNascimento;
+     
+    printf("A sua idade atual é: %d\n", idade);
+    printf("A sua idade em 2050, será de: %d\n", idade2050);
+
+    return 0; 
+
+
